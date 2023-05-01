fa5021beecbd25b90e47359fe5dc9511dabe47b6
Boris Rybalkin <ribalkin@gmail.com>
2023-05-01 19:41:25 +0100

    docker

--- java/build.sh
+++ java/build.sh
@@ -4,13 +4,13 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/java
-while ! docker create --name=syncloud eclipse-temurin:$VERSION-jre; do
+while ! docker create --name=java eclipse-temurin:$VERSION-jre; do
   sleep 1
   echo "retry docker"
 done
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-docker export syncloud -o syncloud.tar
-tar xf syncloud.tar
-rm -rf syncloud.tar
+docker export java -o app.tar
+tar xf app.tar
+rm -rf app.tar
 cp ${DIR}/java.sh ${BUILD_DIR}/bin/
--- nginx/build.sh
+++ nginx/build.sh
@@ -4,13 +4,13 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/java
-while ! docker create --name=syncloud nginx:$VERSION ; do
+while ! docker create --name=nginx nginx:$VERSION ; do
   sleep 1
   echo "retry docker"
 done
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
-docker export syncloud -o syncloud.tar
-tar xf syncloud.tar
-rm -rf syncloud.tar
+docker export nginx -o app.tar
+tar xf app.tar
+rm -rf app.tar
 cp ${DIR}/nginx.sh ${BUILD_DIR}/bin/
