0cb32ca3b6163632c654026c751d92a8def51223
Boris Rybalkin <ribalkin@gmail.com>
2023-05-01 19:23:36 +0100

    java

--- java/java.sh
+++ java/java.sh
@@ -2,4 +2,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 LIBS=$(echo ${DIR}/lib/*-linux-gnu*)
 LIBS=$LIBS:$(echo ${DIR}/opt/java/openjdk/lib)
-${DIR}/lib/*-linux*/ld-*.so --library-path $LIBS ${DIR}/opt/java/openjdk/bin/java "$@"
+${DIR}/lib/*-linux*/ld-*.so* --library-path $LIBS ${DIR}/opt/java/openjdk/bin/java "$@"
