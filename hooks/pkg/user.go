65faca4daea90bf7b41bf48b2016c8918a5af3fe
Boris Rybalkin <ribalkin@gmail.com>
2023-05-02 20:43:35 +0100

    user

--- hooks/pkg/user.go
+++ hooks/pkg/user.go
@@ -11,18 +11,23 @@ func UserExists(username string) bool {
 	return err == nil
 }
 
-func UserCreateIfMissing(username string) error {
+func CreateUser(username string) error {
 	if !UserExists(username) {
-		UserCreate(username)
+		return createUser(username)
 	}
+	return nil
 }
 
-func UserCreate(username string) ([]byte, error) {
+func createUser(username string) error {
 	command := exec.Command("/usr/sbin/useradd",
 		"-r",
 		"-s", "/bin/false",
 		"-m",
 		"-d", fmt.Sprintf("/home/%s", username),
 	)
-	return command.CombinedOutput()
+	output, err := command.CombinedOutput()
+	if err != nil {
+		return fmt.Errorf("%w: %s", err, string(output))
+	}
+	return nil
 }
