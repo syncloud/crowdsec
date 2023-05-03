c307014a371101828fad9340045b0bebf54bc847
Boris Rybalkin <ribalkin@gmail.com>
2023-05-03 21:40:32 +0100

    snap hooks

---
+++ hooks/cmd/configure/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"hooks"
+	"os"
+)
+
+func main() {
+	var rootCmd = &cobra.Command{
+		SilenceUsage: true,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return hooks.New().Configure()
+		},
+	}
+
+	err := rootCmd.Execute()
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+}
