73edea1bd38841b4b952f53dcb278e15d5c6c7ff
Boris Rybalkin <ribalkin@gmail.com>
2023-05-05 21:45:05 +0100

    metabase

--- bin/service.metabase.sh
+++ bin/service.metabase.sh
@@ -3,8 +3,5 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export MB_DB_TYPE=h2
-expor MB_DB_FILE=/var/snap/crowdsec/current/metabase.db/metabase.db
+export MB_DB_FILE=/var/snap/crowdsec/current/metabase.db/metabase.db
 exec ${DIR}/java/bin/java -jar metabase.jar
-
-r
-
