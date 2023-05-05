f320622868752f2dfff1c4f00a2c195be9fd2e04
Boris Rybalkin <ribalkin@gmail.com>
2023-05-05 22:00:22 +0100

    fix nginx

--- bin/service.nginx.sh
+++ bin/service.nginx.sh
@@ -3,4 +3,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 /bin/rm -f /var/snap/crowdsec/common/web.socket
-exec ${DIR}/nginx/usr/sbin/nginx -c /var/snap/crowdsec/current/config/nginx.conf -p ${DIR}/nginx -e stderr
+exec ${DIR}/nginx/bin/nginx.sh -c /var/snap/crowdsec/current/config/nginx.conf -p ${DIR}/nginx -e stderr
