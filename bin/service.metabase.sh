ba58c5c9a34a35c81df21d309191b9e0d6af4781
Boris Rybalkin <ribalkin@gmail.com>
2023-05-06 08:59:10 +0100

    fix java

--- bin/service.metabase.sh
+++ bin/service.metabase.sh
@@ -4,4 +4,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
 export MB_DB_TYPE=h2
 export MB_DB_FILE=/var/snap/crowdsec/current/metabase.db
+export PATH=$PATH:${DIR}/java/bin
 exec ${DIR}/java/bin/java -jar /snap/crowdsec/current/metabase/metabase.jar
