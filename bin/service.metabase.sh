07615ebd26c50036402ea9de941e7d783f93cc6e
Boris Rybalkin <ribalkin@gmail.com>
2023-05-09 08:58:49 +0100

    mb config

--- bin/service.metabase.sh
+++ bin/service.metabase.sh
@@ -2,14 +2,14 @@
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )
 
-MB_DB_TYPE=h2
-MB_DB_FILE=/var/snap/crowdsec/current/metabase.db
-MB_LDAP_ENABLED=true
-MB_LDAP_HOST=localhost
-MB_LDAP_BIND_DN="cn=admin,dc=syncloud,dc=org"
-MB_LDAP_PASSWORD=syncloud
-MB_LDAP_USER_BASE="ou=users,dc=syncloud,dc=org"
-MB_LDAP_USER_FILTER="(&(objectClass=inetOrgPerson)(cn={login}))"
-MB_JETTY_PORT=3001
+export MB_DB_TYPE=h2
+export MB_DB_FILE=/var/snap/crowdsec/current/metabase.db
+export MB_LDAP_ENABLED=true
+export MB_LDAP_HOST=localhost
+export MB_LDAP_BIND_DN="cn=admin,dc=syncloud,dc=org"
+export MB_LDAP_PASSWORD=syncloud
+export MB_LDAP_USER_BASE="ou=users,dc=syncloud,dc=org"
+export MB_LDAP_USER_FILTER="(&(objectClass=inetOrgPerson)(cn={login}))"
+export MB_JETTY_PORT=3001
 export PATH=$PATH:${DIR}/java/bin
 exec ${DIR}/java/bin/java -jar /snap/crowdsec/current/metabase/metabase.jar
