640611b2bbf9d4069cb707a151145f94b98a3246
Boris Rybalkin <ribalkin@gmail.com>
2023-05-11 20:01:40 +0100

    try java8

--- metabase/build.sh
+++ metabase/build.sh
@@ -18,7 +18,8 @@ rm metabase_sqlite.zip
 cd $DIR
 H2=1.4.199
 wget https://repo1.maven.org/maven2/com/h2database/h2/$H2/h2-$H2.jar
-apk add openjdk11
+apk add openjdk8
 java -jar h2-$H2.jar -webAllowOthers -tcpAllowOthers &
 java -cp h2-$H2.jar org.h2.tools.Shell -url "jdbc:h2:tcp://localhost/$BUILD_DIR/metabase.db/metabase.db" -sql "update METABASE_DATABASE set details = '{\"db\":\"/var/snap/crowdsec/current/crowdsec.db\"}'"
 kill $(jobs -p)
+
