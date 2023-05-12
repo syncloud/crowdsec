2ab51d28cada1bf2a076e73ca6fae0f89aabae56
Boris Rybalkin <ribalkin@gmail.com>
2023-05-12 18:44:59 +0100

    dovecot collection

--- crowdsec/build.sh
+++ crowdsec/build.sh
@@ -4,10 +4,11 @@ DIR=$( cd "$( dirname "$0" )" && pwd )
 cd ${DIR}
 VERSION=$1
 BUILD_DIR=${DIR}/../build/snap/crowdsec
-while ! docker create --name=crowdsec crowdsecurity/crowdsec:v$VERSION; do
+while ! docker build --build-arg VERSION=$VERSION -t crowdsec . ; do
   sleep 1
   echo "retry docker"
 done
+docker create --name=crowdsec crowdsec
 mkdir -p ${BUILD_DIR}
 cd ${BUILD_DIR}
 docker export crowdsec -o crowdsec.tar
--- metabase/build.sh
+++ metabase/build.sh
@@ -21,5 +21,6 @@ wget https://repo1.maven.org/maven2/com/h2database/h2/$H2/h2-$H2.jar
 apk add openjdk8
 java -jar h2-$H2.jar -webAllowOthers -tcpAllowOthers &
 java -cp h2-$H2.jar org.h2.tools.Shell -url "jdbc:h2:tcp://localhost/$BUILD_DIR/metabase.db/metabase.db" -sql "update METABASE_DATABASE set details = '{\"db\":\"/var/snap/crowdsec/current/crowdsec.db\"}'"
+java -cp h2-$H2.jar org.h2.tools.Shell -url "jdbc:h2:tcp://localhost/$BUILD_DIR/metabase.db/metabase.db" -sql "shutdown compact"
 kill $(jobs -p)
 
